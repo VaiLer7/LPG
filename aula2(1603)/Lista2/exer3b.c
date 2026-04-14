5940c7f84fe14e7b0038fe6993749635763e72a5
Vailer <hugo73852@email.com>
2026-04-13 22:17:19 -0300

    exercicios feitos: - Série de Nilakantha - Fibonacci (vetor)

--- aula2(1603)/Lista2/exer3b.c
+++ aula2(1603)/Lista2/exer3b.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 //Implemente as duas formas para calcular o valor aproximado de π, conforme as séries a seguir. O número de termos é definido pelo usuário.
 //Série de Nilakantha:
+
+int divisor(int termo){
+    int comeco=termo*2;
+    comeco=(comeco)*(comeco+1)*(comeco+2);
+
+    return comeco;
+}
+double calc_fracao(int termo){
+    return (double)4.0/(divisor(termo));
+}
 int main(){
-    
+    int termos, mult=1;
+    double fracao, pi=3;
+
+    printf("Quantos termos deseja que tenha de precisao: ");
+    scanf("%d", &termos);
+
+    for(int i=1; i<termos; i++){
+        fracao=calc_fracao(i);
+        pi+=mult*fracao;
+        mult=-mult;
+    }
+
+    printf("\nValor aproximado de PI: %lf", pi);
+
+    getchar();
+    getchar();
 }
--- aula3(2303)/Lista3/exer6.c
+++ aula3(2303)/Lista3/exer6.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 //Faça um programa que mostre na tela os n primeiros termos da sequência de Fibonacci. Por exemplo, dado n = 8, temos: 1, 1, 2, 3, 5, 8, 13 e 21. A determinação do n-ésimo termo da sequência deve ser feita por uma função iterativa que tem o seguinte protótipo:
 //int fibo(int n);
-int main(){
 
+int fibo(int termos){
+    int vetor[termos];
+    vetor[-1]=0;
+    vetor[0]=1;
+    printf("1 ");
+    for(int i=1; i<termos; i++){
+        vetor[i]=vetor[i-1]+vetor[i-2];
+        printf("%d ", vetor[i]);
+    }
+    return vetor[termos-1];
+}
+
+int main(){
+    int termos, nesimo;
+
+    printf("Digite quantos numeros deseja mostrar na tela da sequencia Fibonacci(>1): ");
+    scanf("%d", &termos);
+
+    nesimo=fibo(termos);
+    printf("\nValor do n-esimo termo eh: %d", nesimo);
+
+    getchar();
+    getchar();
 }
