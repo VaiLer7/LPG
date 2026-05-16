cf25173ae75df7e5b8b16fb46fdf7c7ad720bde0
VaiLer7 <hugo73852@gmail.com>
2026-05-15 22:58:02 -0800

    Exercicio 9 resolvido

--- aula6(1105)/Lista6/exer9.c
+++ aula6(1105)/Lista6/exer9.c
@@ -12,30 +12,29 @@ int main(){
     printf("digite a frase: ");
     getchar();
     fgets(texto, tam, stdin);
+    texto[strlen(texto)-1]=' '; //substituindo o '\n' por espaço (' ') para melhor visualização
+    printf("|%s|", texto);
 
     trim(texto);
 
-    printf("\no mesmo texto sem os espaços iniciais e finais:\n%s", texto);
+    printf("\no mesmo texto sem os espaços iniciais e finais:\n|%s|\n\n", texto);
 }
 
 void trim(char texto[]){
-    printf("\n\n\n%s", texto);
-    for(int i=0; texto[i]==' '; i++){
-        if(texto[i+1]!=' ' && i>0){
-            int j=0;
-            for(; texto[j]!='\0'; j++){
-                texto[j]=texto[i+j+1];
-            }
-            texto[j]='\0';
+    int i=0;
+    for(; texto[i]==' '; i++);
+    if(i>0){
+        int j=0;
+        for(; texto[i+j]!='\0'; j++){
+            texto[j]=texto[i+j];
         }
+        texto[j]='\0';
     }
-    printf("\n\n\n%s", texto);
 
     int tam = strlen(texto)-2; //'-2' para tirar o "\0" e "\n"
-    for(int i=tam; texto[i]==' '; i--){
-        if(texto[i-1]!=' ' && i<tam){
-            texto[i]='\0';
-        }
+    i=tam;
+    for(; texto[i]==' '; i--);
+    if(i<tam){
+        texto[i+1]='\0';
     }
-    printf("\n\n\n%s", texto);
 }
